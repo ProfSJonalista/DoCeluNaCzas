b7ab96975ec68c278a3944d1af345e1ebed3ba14
Jan Miętki <jan.mietki@gmail.com>
2018-06-14 22:06:32 +0200

    Add project files.

---
+++ DoCeluNaCzas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoCeluNaCzas.MvcApplication" Language="C#" %>
---
+++ DoCeluNaCzas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoCeluNaCzas.Startup))]
+namespace DoCeluNaCzas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
