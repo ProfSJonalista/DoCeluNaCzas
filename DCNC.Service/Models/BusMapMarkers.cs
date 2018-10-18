a2e932c90d98548729389a7a45fe9aedac74ec8b
Sinnlos <sinnlos@github.com>
2018-10-18 17:53:43 +0200

    Marker model

---
+++ DCNC.Service/Models/BusMapMarkers.cs
@@ -0,0 +1,28 @@
+﻿using DoCeluNaCzas.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DCNC.Service.Models
+{
+    public class BusMapMarkers
+    {
+
+        public class MarkerList
+        {
+
+            public List<Marker> markers { get; set; }
+
+
+        }
+
+        public class Marker
+        {
+            public string StopId { get; set; }
+            public string StopDesc { get; set; }
+            public string StopLat { get; set; }
+            public string StopLon { get; set; }
+        }
+    }
+}
