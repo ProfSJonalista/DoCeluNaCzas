a4aa2818249b238397141ba743132c963bcc9365
Sinnlos <sinnlos@github.com>
2018-11-22 21:31:29 +0100

    markes added on the map, map on the main page

--- DCNC.Data/Helpers/Constants.cs
+++ DCNC.Data/Helpers/Constants.cs
@@ -6,7 +6,7 @@ namespace DoCeluNaCzas.DataAccess.Helpers
 {
     public class Constants
     {
-        public static string BUS_STOPS = "http://docelunaczaswebapi.azurewebsites.net/api/BusStop";
+        public static string BUS_STOPS = "http://localhost:58893/api/BusStop?hasData=false";
         public static string BUS_LINES = "http://docelunaczaswebapi.azurewebsites.net/api/BusLine";
     }
 }
