2c051ea3a73869d70b4e555d5abff1bfdddd53ef
Jan Miętki <jan.mietki@gmail.com>
2019-04-05 20:21:53 +0200

    cache service fix

--- DoCeluNaCzas/Service/Cashing/CacheService.cs
+++ DoCeluNaCzas/Service/Cashing/CacheService.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-//using System.Runtime.Caching;
+using System.Runtime.Caching;
 using System.Web;
 
 namespace DoCeluNaCzas.Service.Cashing
 {
     public class CacheService
     {
+        static readonly ObjectCache _cache = MemoryCache.Default;
 
-        //static readonly ObjectCache _cache = MemoryCache.Default;
+        public void CacheData<T>(T data, string cacheKey)
+        {
+            _cache.Set(cacheKey, data, new CacheItemPolicy());
+        }
 
-        //public void CacheData<T>(T data, string cacheKey)
-        //{
-        //    _cache.Set(cacheKey, data, new CacheItemPolicy());
-        //}
+        public T GetData<T>(string cacheKey) where T : class
+        {
+            return _cache[cacheKey] as T;
+        }
 
-        //public T GetData<T>(string cacheKey) where T : class
-        //{
-        //    return _cache[cacheKey] as T;
-        //}
-
-        //public DateTime GetData(string cacheKey)
-        //{
-        //    return _cache[cacheKey] is DateTime ? (DateTime)_cache[cacheKey] : new DateTime();
-        //}
+        public DateTime GetData(string cacheKey)
+        {
+            return _cache[cacheKey] is DateTime ? (DateTime)_cache[cacheKey] : new DateTime();
+        }
     }
 }
