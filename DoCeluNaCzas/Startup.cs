19eab30d088e44d398daf5b63ff5f62fadfcd1bc
Sinnlos <sinnlos@github.com>
2019-07-31 23:29:41 +0200

    SignalR vs. Yoona #2

--- DoCeluNaCzas/Startup.cs
+++ DoCeluNaCzas/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(DoCeluNaCzas.Startup))]
@@ -9,26 +10,10 @@ namespace DoCeluNaCzas
     {
         public void Configuration(IAppBuilder app)
         {
-            // Branch the pipeline here for requests that start with "/signalr"
-            app.Map("/signalr", map =>
-            {
-                // Setup the CORS middleware to run before SignalR.
-                // By default this will allow all origins. You can 
-                // configure the set of origins and/or http verbs by
-                // providing a cors options with a different policy.
-                
-                var hubConfiguration = new HubConfiguration
-                {
-                    // You can enable JSONP by uncommenting line below.
-                    // JSONP requests are insecure but some older browsers (and some
-                    // versions of IE) require JSONP to work cross domain
-                    // EnableJSONP = true
-                };
-                // Run the SignalR pipeline. We're not using MapSignalR
-                // since this branch already runs under the "/signalr"
-                // path.
-                map.RunSignalR(hubConfiguration);
-            });
+            // Any connection or hub wire up and configuration should go here
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR(new HubConfiguration { EnableJSONP = true });
+            ConfigureAuth(app);
         }
     }
 }
